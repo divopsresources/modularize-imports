61cc8a5eab0bae74bc1e2d4dd54d9d6ab3a22eb4
divopsresources <divopscloud@protonmail.com>
2022-04-16 09:08:02 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-epbswx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-epbswx)
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import LeftHalf from '../components/halves/LeftHalf';
+import RightHalf from '../components/halves/RightHalf';
+
+const Index = () => (
+  <div>
+    <LeftHalf />
+    <RightHalf />
+  </div>
+);
+
+export default Index;
